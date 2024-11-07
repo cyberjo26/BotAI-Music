e0b02955d97c2ddeb301e753410956e83070701f
Johan Adriana Martin <56462946+cyberjo26@users.noreply.github.com>
2024-11-07 13:49:59 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,6 +1,9 @@
 import discord
 from discord.ext import commands
 import os
+import discord.opus
+
+
 
 async def load_cogs(bot):
     for filename in os.listdir("./cogs"):
@@ -13,6 +16,9 @@ bot = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 async def on_ready():
     print(f'Logged in as {bot.user}')
     await load_cogs(bot)
+
+if not discord.opus.is_loaded():
+    discord.opus.load_opus('libopus.so.0')
 
 # Memuat token dari environment variable
 bot.run(os.getenv("DISCORD_TOKEN"))
